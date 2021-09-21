aaed9995e42966b875af1f58b5b4f2dc07c3aaca
paramithatm <paramitha@tokopedia.com>
2021-09-21 20:29:07 +0700

    Init

---
+++ android/app/src/main/kotlin/com/example/something_cute/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.something_cute
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/collection.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Collection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Something",
+        home: Scaffold(
+          backgroundColor: Colors.blue[200],
+          body: Text("something cute"),
+        ));
+  }
+}
